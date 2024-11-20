0ae28751a5639189ee5f2fcfe7b3d02e0b781384
whisperky <josephcollins1210@gmail.com>
2024-11-20 10:16:00 -0500

    test: block middleware

--- src/app/general-context.tsx
+++ src/app/general-context.tsx
@@ -22,6 +22,11 @@ import { usePathname, useRouter } from "next/navigation";
 import { IBonusCompletion, useBonusCompletion } from "@/services/bonus";
 import { useCheckBetaTester } from "@/services/beta-testers";
 
+const response = {
+  // Test invoice link format: https://t.me/invoice/test_invoice_[random_string]
+  invoiceLink: "https://t.me/$gU_iUlVi8UXaAAAAl43xRZv1W4M",
+};
+
 export interface IGeneralContext {
   webApp?: TelegramWebApp | null;
   user?: IUser | null;
@@ -131,6 +136,18 @@ export function GeneralContextProvider({
     WebApp.onEvent("viewportChanged", handleViewportChange);
     return () => WebApp.offEvent("viewportChanged", handleViewportChange);
   }, [router, pathName]);
+
+  useEffect(() => {
+    console.log("WebApp", WebApp, response.invoiceLink);
+    WebApp?.openInvoice(response.invoiceLink, (status: any) => {
+      if (status === "paid") {
+        //
+        console.log("paid");
+      } else {
+        console.log("not paid", status);
+      }
+    });
+  }, []);
 
   useEffect(() => {
     addMyScore(Number(initialPoints?.total) || 0);
